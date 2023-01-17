479f6a6593a5ebbe27da26f88236782ebb515464
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-01-17 14:39:53 +0400

    deleted data by using Id

---
+++ API2Data.sql
@@ -0,0 +1,5 @@
+Use APIDataBase;
+CREATE TABLE restCountries( id int NOT NULL AUTO_INCREMENT, capital VARCHAR(35) ,population Integer ,region VARCHAR(35) ,subregion VARCHAR(35),eng VARCHAR(35) ,
+PRIMARY KEY(id));
+
+Select *From restCountries;
---
+++ src/main/java/deleteData.java
@@ -0,0 +1,50 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Scanner;
+
+import com.mysql.jdbc.Statement;
+
+public class deleteData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String url = "jdbc:mysql://localhost:3306/APIDataBase";
+
+		// Username and password to access DB
+		// Custom initialization
+		String user = "root";
+		String pass = "root";
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter id: ");
+		Integer idInput = scanner.nextInt();
+
+		String sql2 = "delete from restCountries where id ='"+idInput + "'";
+		Connection con1 = null;
+
+		try {
+
+			Driver driver = (Driver) Class.forName("com.mysql.jdbc.Driver").newInstance();
+			// Registering drivers
+			DriverManager.registerDriver(driver);
+
+			// Reference to connection interface
+			con1 = DriverManager.getConnection(url, user, pass);
+
+			// Creating a statement
+			Statement st = (Statement) con1.createStatement();
+			int m = st.executeUpdate(sql2);
+
+		}
+
+		// Catch block to handle exceptions
+		catch (Exception ex) {
+			// Display message when exceptions occurs
+			System.err.println(ex);
+		}
+
+	}
+
+
+}
